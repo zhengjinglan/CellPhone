833f02f9d77694ec7bc29790f416370db6d30719
lqx <15032192873@163.com>
2018-06-14 16:13:10 +0800

    订单相关

---
+++ src/main/java/com/aaa/service/OrderService.java
@@ -0,0 +1,27 @@
+package com.aaa.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.aaa.dao.OrderMapper;
+import com.aaa.entity.Order;
+import com.aaa.entity.OrderExample;
+
+@Service
+public class OrderService {
+	
+	@Autowired
+	OrderMapper maper;
+	
+	public int add(Order o){
+		return maper.insert(o);
+	}
+	public List<Order> queryOrder(Order o){
+		OrderExample order=new OrderExample();
+		return maper.selectByExample(order);
+		
+	}
+
+}
