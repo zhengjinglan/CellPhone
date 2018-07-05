2aa8f8a2a9a94954153f01ee6a4c97da40d049e3
xu2604980 <calmer_xu@163.com>
2018-07-05 17:39:05 +0800

    remove unused `import`

--- src/main/java/com/aaa/controller/ConsumeItemTypeController.java
+++ src/main/java/com/aaa/controller/ConsumeItemTypeController.java
@@ -2,8 +2,6 @@ package com.aaa.controller;
 
 import java.util.List;
 
-import net.sf.json.JSONArray;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,31 +17,35 @@ import com.aaa.util.StrUtil;
 public class ConsumeItemTypeController {
     @Autowired
     ConsumeItemTypeService service;
-    
+
     @RequestMapping("queryPage")
     @ResponseBody
-    public EasyuiPage queryPage(Integer page, Integer rows){
-        return new EasyuiPage(service.queryPage(page, rows),service.getCount());
+    public EasyuiPage queryPage(Integer page, Integer rows) {
+        return new EasyuiPage(service.queryPage(page, rows), service.getCount());
     }
+
     @RequestMapping("add")
     @ResponseBody
-    public boolean add(ConsumeItemType type){
-        return service.add(type)>0?true:false;
-    }   
+    public boolean add(ConsumeItemType type) {
+        return service.add(type) > 0 ? true : false;
+    }
+
     @RequestMapping("update")
     @ResponseBody
-    public int update(ConsumeItemType type){
+    public int update(ConsumeItemType type) {
         return service.update(type);
     }
+
     @RequestMapping("dels")
     @ResponseBody
-    public boolean dels(String ids){
+    public boolean dels(String ids) {
         String[] strs = ids.split(",");
         return service.remove(StrUtil.parseToInteger(strs));
     }
+
     @RequestMapping("queryAll")
     @ResponseBody
-    public List<ConsumeItemType> queryAll(){
+    public List<ConsumeItemType> queryAll() {
         return service.list();
     }
-}  
+}
